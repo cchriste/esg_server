c48b2b170caf6fe368f3e043a8d80bab8b3b4efd
root <spetruzza@sci.utah.edu>
2019-06-27 21:28:24 +0000

    use env variable to locate local.json

--- code/map_files.py
+++ code/map_files.py
@@ -10,10 +10,12 @@ sample_table = { 'esg_dataroot' : '/path/to/data',
 		  'css03_data': '/path/to/css03/data', 'user_pub_work': '/path/to/user/data' }
 
 # use local configuration if exists
-if path.exists('/home/ondemand/conf/local.json'):
-    with open('/home/ondemand/conf/local.json') as json_file:
+if path.exists(os.environ['ONDEMAND_HOME']+'/conf/local.json'):
+    with open(os.environ['ONDEMAND_HOME']+'/conf/local.json') as json_file:
         sample_table = json.load(json_file)[0]
-    
+else:
+    print("WARNING: $ONDEMAND_HOME/conf/local.json not found! Won't be able to resolve file paths") 
+        
 def get_mapped_dataset(id):
 
 	search_url = "https://esgf-node.llnl.gov/esg-search/search/?type=File&dataset_id={}&format=application%2fsolr%2bjson".format(id)
